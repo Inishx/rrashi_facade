4149b72e432fd16747c66482a78974bc6474f1f7
Inishx <127223243+Inishx@users.noreply.github.com>
2024-09-26 23:00:45 +0200

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -30,7 +30,7 @@
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Votre message a Ã©tÃ© envoyÃ©. </strong>");
+                            .append("<strong>Votre message a été envoyé. </strong>");
                     $('#success > .alert-success')
                             .append('</div>');
                     $('#contactForm').trigger("reset");
